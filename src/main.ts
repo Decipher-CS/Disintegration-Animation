7bab74ed31eb0c6a924d542e5d00303a5b2436b3
de-sipher <0mu5sfm4@anonaddy.me>
2022-11-12 18:16:39 +0530

    added bg gradient transition | added border | fixed bug where image gets cropped in half when the browser window is not maximized

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 // Tutorial To Follow:
 // https://www.youtube.com/watch?v=vAJEHf92tV0
 window.addEventListener('load', function () {
-    console.log('Page Loaded')
     const gameBoard = document.getElementById('game_board') as HTMLCanvasElement
     const ctx = gameBoard!.getContext('2d')
-    gameBoard.height = window.innerHeight
-    gameBoard.width = window.innerWidth
+    gameBoard.height = window.innerHeight - 100
+    gameBoard.width = window.innerHeight - 100
 
     interface Particle {
-        particlelocationX: number
+        particleLocationX: number
         particleLocationY: number
         size: number
         effect: Effect
@@ -29,29 +28,31 @@ window.addEventListener('load', function () {
     class Particle {
         constructor(effect: Effect, x: number, y: number, color: string) {
             this.effect = effect
-            this.particlelocationX = Math.random() * this.effect.width
+            this.particleLocationX = Math.random() * this.effect.width
             this.particleLocationY = Math.random() * this.effect.height
+            // this.particleLocationX = x
+            // this.particleLocationY = y
             this.originX = Math.floor(x)
             this.originY = Math.floor(y)
             this.color = color
             this.size = this.effect.gap
-            this.vy = 0
             this.vx = 0
+            this.vy = 0
+            this.ease = 0.1
             this.dx = 0
             this.dy = 0
             this.distance = 0
             this.force = 0
             this.angle = 0
-            this.friction = 0.95
-            this.ease = 0.2
+            this.friction = 0.8
         }
         draw(context: CanvasRenderingContext2D | null) {
             context && (context.fillStyle = this.color)
-            context?.fillRect(this.particlelocationX, this.particleLocationY, this.size, this.size)
+            context?.fillRect(this.particleLocationX, this.particleLocationY, this.size, this.size)
         }
         update() {
-            this.dx = this.effect.mouse.x! - this.particlelocationX
-            this.dy = this.effect.mouse.y! - this.particleLocationY
+            this.dx = this.effect.mouse.x - this.particleLocationX
+            this.dy = this.effect.mouse.y - this.particleLocationY
             this.distance = this.dx * this.dx + this.dy * this.dy
             this.force = -this.effect.mouse.radius / this.distance
 
@@ -61,12 +62,13 @@ window.addEventListener('load', function () {
                 this.vy += this.force * Math.sin(this.angle)
             }
 
-            this.particlelocationX += (this.vx *= this.force) + (this.originX - this.particleLocationY) * this.ease
-            this.particleLocationY += (this.vy *= this.force)+ (this.originY - this.particleLocationY) * this.ease
+            this.particleLocationX += (this.vx *= this.friction) + (this.originX - this.particleLocationX) * this.ease
+            this.particleLocationY += (this.vy *= this.friction) + (this.originY - this.particleLocationY) * this.ease
         }
         distort() {
-            this.particlelocationX = Math.random() * this.effect.width
+            this.particleLocationX = Math.random() * this.effect.width
             this.particleLocationY = Math.random() * this.effect.height
+            this.ease = 0.1
         }
     }
 
@@ -82,8 +84,8 @@ window.addEventListener('load', function () {
         gap: number
         mouse: {
             radius: number
-            x: number | undefined
-            y: number | undefined
+            x: number
+            y: number
         }
     }
     class Effect {
@@ -96,20 +98,22 @@ window.addEventListener('load', function () {
             this.imageX = this.centerX - this.image.width * 0.5
             this.imageY = this.centerY - this.image.height * 0.5
             this.particleArray = []
-            this.gap = 7
+            this.gap = 3
             this.mouse = {
                 radius: 3000,
-                x: undefined,
-                y: undefined,
+                x: 0,
+                y: 0,
             }
             window.addEventListener('mousemove', (e) => {
-                this.mouse.x = e.x
-                this.mouse.y = e.y
+                const offsetX = (window.innerWidth - this.width) / 2
+                const offsetY = (window.innerHeight - this.height) / 2
+                this.mouse.x = e.x - offsetX
+                this.mouse.y = e.y - offsetY
             })
         }
         init(context: CanvasRenderingContext2D | null) {
             context?.drawImage(this.image, this.imageX, this.imageY)
-            const pixels = context?.getImageData(0, 0, this.width, this.height).data!
+            const pixels = context!.getImageData(0, 0, this.width, this.height).data
             for (let y = 0; y < this.height; y += this.gap) {
                 for (let x = 0; x < this.height; x += this.gap) {
                     const index = (y * this.width + x) * 4
